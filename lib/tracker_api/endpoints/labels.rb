93c681a262e084aeb35aafffdf6fb919a9db61ab
Julie Silvestri <julie.silvestri@gmail.com>
2015-07-23 17:11:01 -0400

    Fix ability to add a single label to a story

--- lib/tracker_api/endpoints/labels.rb
+++ lib/tracker_api/endpoints/labels.rb
@@ -22,10 +22,7 @@ module TrackerApi
         Resources::Label.new({ project_id: project_id }.merge(data))
       end
 
-      def add_to_story(project_id, story_id, label_name = nil, params={})
-        unless params['name'] || params[:name]
-          params[:name] = label_name
-        end
+      def add_to_story(project_id, story_id, params={})
         data = client.post("/projects/#{project_id}/stories/#{story_id}/labels", params: params).body
 
         Resources::Label.new({ project_id: project_id }.merge(data))
